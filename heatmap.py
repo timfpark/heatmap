e24c42ead153d28c3aadb9f02e63596c12e1a64c
Tim Park <timfpark@gmail.com>
2017-06-15 11:18:15 -0700

    update heatmap to use streaming approach

--- heatmap.py
+++ heatmap.py
@@ -1,28 +1,35 @@
-from azure.storage.blob import BlobService
 from datetime import date, datetime, timedelta
 import json
 import math
+import calendar
 import os
-import sys
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+import sys
 from tile import Tile
 import time
 
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+from cassandra.cluster import Cluster
+
+CASSANDRA_ENDPOINT = os.environ["CASSANDRA_ENDPOINT"]
+DETAIL_ZOOM_DELTA = 5
 MAX_ZOOM_LEVEL = 16
+BASE_VALUE = 100
 
 KEY_FIELD = 0
 VALUE_FIELD = 1
 
-HEATMAP_CONTAINER = 'heatmaps'
-
-def json_loader(line):
-    try:
-        location = json.loads(line)
-        location['datetime'] = datetime.strptime(location["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ")
-        location['timestamp'] = time.mktime(location['datetime'].timetuple())
-        return [ location ]
-    except ValueError, e:
-        return []
+def dataframe_loader(row):
+    tileId = Tile.tile_id_from_lat_long(row["latitude"], row["longitude"], MAX_ZOOM_LEVEL + DETAIL_ZOOM_DELTA)
+    return {
+        "tileId": tileId,
+        "timestamp": calendar.timegm(row["timestamp"].utctimetuple()) * 1000,
+        "userId": row["user_id"],
+        "count": 1.0
+    }
 
 def build_timespan_label(timespanType, localDate):
     month = str(localDate.month)
@@ -43,124 +50,156 @@ def build_timespan_label(timespanType, localDate):
 def build_tile_composite_key(userId, tileId, timespanLabel):
     return userId + "/" + timespanLabel + "/" + tileId
 
-def tile_id_timespans_mapper(location):
-    tileTimespanMappings = []
-    tileId = Tile.tile_id_from_lat_long(location['latitude'], location['longitude'], MAX_ZOOM_LEVEL)
-    tileIds = Tile.tile_ids_for_all_zoom_levels(tileId)
-    for timespanType in ["alltime", "year", "month"]:
-        timespanLabel = build_timespan_label(timespanType, location['datetime'])
-        for tileId in tileIds:
-            tile = Tile.tile_from_tile_id(tileId)
+def tile_id_timespans_mapper_for_zoom(zoom):
+    def tile_id_timespans_mapper(location):
+        tileTimespanMappings = []
+        oldTile = Tile.tile_from_tile_id(location['tileId'])
+        tileId = Tile.tile_id_from_lat_long(oldTile.center_latitude, oldTile.center_longitude, zoom)
+        for timespanType in ["alltime"]: # , "year", "month"
+            timespanLabel = "alltime" # build_timespan_label(timespanType, location['datetime'])
             userGroups = ['all']
             if not location['userId'][:1] == 'x':
                 userGroups.append(location['userId'])
             for userId in userGroups:
                 tileTimespanMappings.append((
                     build_tile_composite_key(userId, tileId, timespanLabel),
-                    1
+                    location['count']
                 ))
-    return tileTimespanMappings
+            return tileTimespanMappings
+    return tile_id_timespans_mapper
 
-def key_by_result_set(entry):
-    # Zoom ranges for resultsets: A tile at zoom N will be in zoom level N-RESULTSET_DELTA_LOW to N-RESULTSET_DELTA_HIGH resultsets.
-    RESULTSET_DELTA_LOW = 5
-    RESULTSET_DELTA_HIGH = 4
-    keyFields = entry[KEY_FIELD].split('/')
-    if len(keyFields) != 3:
-        return []
-    userGroup = keyFields[0]
-    timespanLabel = keyFields[1]
-    tileId = keyFields[2]
-    resultSet = []
+def map_to_resultset(bucket):
+    bucketKeyParts = bucket[KEY_FIELD].split('/')
+    userId = bucketKeyParts[0]
+    timespanLabel = bucketKeyParts[1]
+    tileId = bucketKeyParts[2]
     tile = Tile.tile_from_tile_id(tileId)
-    lowLevel = max(0, tile.zoom - RESULTSET_DELTA_LOW)
-    highLevel = max(0, tile.zoom - RESULTSET_DELTA_HIGH)
-    if tile.zoom >= MAX_ZOOM_LEVEL-1:
-        highLevel = MAX_ZOOM_LEVEL-1
-    if tile.zoom == MAX_ZOOM_LEVEL:
-        highLevel = MAX_ZOOM_LEVEL
-    for zoomLevel in range(lowLevel, highLevel):
-        bucketTileId = Tile.tile_id_from_lat_long(tile.center_latitude, tile.center_longitude, zoomLevel)
-        key = build_tile_composite_key(userGroup, bucketTileId, timespanLabel)
-        print key
-        resultSet.append(
-            (key, entry)
-        )
-    return resultSet
+    result = {
+        "tileId": tileId,
+        "count": bucket[VALUE_FIELD]
+    }
+    resultSetTileId = Tile.tile_id_from_lat_long(tile.center_latitude, tile.center_longitude, tile.zoom - DETAIL_ZOOM_DELTA)
+    return (build_tile_composite_key(userId, resultSetTileId, timespanLabel), result)
 
-def tuple_loader(line):
-    return [eval(line)]
+def heatmap_to_locations(bucket):
+    locations = []
+    bucketKeyParts = bucket[KEY_FIELD].split('/')
+    userId = bucketKeyParts[0]
+    timespan = bucketKeyParts[1]
+    heatmap = bucket[VALUE_FIELD]
+    for tileId in heatmap:
+        locations.append({
+            "userId": userId,
+            "count": heatmap[tileId],
+            "tileId": tileId,
+            "timespan": timespan
+        })
+    return locations
 
-def combine_result_sets(resultsetValues):
-    combined = {}
-    for resultset in list(resultsetValues):
-        for tuple in resultset:
-            key = tuple[KEY_FIELD]
-            if key in combined:
-                combined[key] += tuple[VALUE_FIELD]
-            else:
-                combined[key] = tuple[VALUE_FIELD]
-    combinedResultSet = []
-    for key in combined:
-        combinedResultSet.append(
-           (key, combined[key])
-        )
-    return combinedResultSet
+def apply_base_value(heatmap_value):
+    return heatmap_value + BASE_VALUE
 
-def write_result_set_into_blob_storage(iterator):
-    for row in iterator:
-        keyParts = row[KEY_FIELD].split('/')
-        tileParts = keyParts[2].split('_')
-        resultSetZoomLevel = int(tileParts[0])
-        tileTimePeriodResultSet = {
-            "heatmap": []
-        }
-        for item in row[VALUE_FIELD]:
-            keyParts = item[KEY_FIELD].split('/')
-            tileTimePeriodResultSet["heatmap"].append({
-                "tc": item[VALUE_FIELD],
-                "t": keyParts[2]
-            })
-        successful = False
-        while not successful:
-            try:
-                blobService.put_blob(HEATMAP_CONTAINER, row[KEY_FIELD], json.dumps(tileTimePeriodResultSet), "BlockBlob", x_ms_blob_cache_control="max-age=3600", x_ms_blob_content_type="application/json")
-                successful = True
-            except:
-                print "error putting heatmap: ", sys.exc_info()[0]
-                continue
-    yield None
+def build_heatmaps(locations):
+    heatmaps = None
+    for zoom in range(MAX_ZOOM_LEVEL + DETAIL_ZOOM_DELTA, DETAIL_ZOOM_DELTA, -1):
+        mapper = tile_id_timespans_mapper_for_zoom(zoom)
+        countByTileIdTimespanKey = locations.flatMap(mapper).reduceByKey(lambda total1, total2: total1 + total2).mapValue(apply_base_value)
+        zoomHeatmap = countByTileIdTimespanKey.map(map_to_resultset).groupByKey().mapValues(list_to_dict)
+        if not heatmaps:
+            heatmaps = zoomHeatmap
+        else:
+            heatmaps = heatmaps.union(zoomHeatmap)
+        locations = zoomHeatmap.flatMap(heatmap_to_locations)
+    return heatmaps
 
-blobService = BlobService(account_name=os.environ["LOCATION_STORAGE_ACCOUNT"], account_key=os.environ["LOCATION_STORAGE_KEY"])
+def list_to_dict(heatmapList):
+    heatmap = {}
+    for entry in heatmapList:
+        tileId = entry['tileId']
+        count = entry['count']
+        heatmap[tileId] = count
+    return heatmap
 
-blobService.create_container(HEATMAP_CONTAINER)
-blobService.set_container_acl(HEATMAP_CONTAINER, x_ms_blob_public_access='container')
+def heatmap_to_json(heatmap):
+    return json.dumps(heatmap)
 
-def check_config():
-    if not "LOCATION_STORAGE_ACCOUNT" in os.environ:
-        print "Required environment variable LOCATION_STORAGE_ACCOUNT missing."
+def kafka_json_loader(tuple):
+    try:
+        location = json.loads(tuple[VALUE_FIELD])
+        if location and location['latitude'] and location['longitude']:
+            location['tileId'] = Tile.tile_id_from_lat_long(location["latitude"], location["longitude"], MAX_ZOOM_LEVEL + DETAIL_ZOOM_DELTA)
+            location['count'] = 1.0
+            return [ location ]
+        else:
+            print "no location or latitude or longitude"
+            return []
+    except ValueError, e:
+        print "exception: " + str(tuple)
+        return []
 
-    if not "LOCATION_STORAGE_KEY" in os.environ:
-        print "Required environment variable LOCATION_STORAGE_KEY missing."
+def merge_partial_heatmap(session, heatmapPartial):
+    id = heatmapPartial[KEY_FIELD]
+    heatmapPartial = heatmapPartial[VALUE_FIELD]
+    mergedHeatmap = {}
+    rows = session.execute("SELECT id, heatmap FROM rhom.heatmaps WHERE id='" + id + "'")
+    for row in rows:
+        mergedHeatmap = json.loads(row[1])
+    for tileId in heatmapPartial:
+        if tileId in mergedHeatmap:
+            mergedHeatmap[tileId] = mergedHeatmap[tileId] + heatmapPartial[tileId]
+        else:
+            mergedHeatmap[tileId] = heatmapPartial[tileId]
+    print "updating " + id
+    session.execute("INSERT INTO rhom.heatmaps (id, heatmap) VALUES (%s, %s)", (id, json.dumps(mergedHeatmap)))
 
-    if not "LOCATIONS_ROOT" in os.environ:
-        print "Required environment variable LOCATIONS_ROOT missing."
+def merge_partial_heatmap_rdd(heatmapPartialRDD):
+    cluster = Cluster([CASSANDRA_ENDPOINT])
+    session = cluster.connect()
+    for heatmapPartial in heatmapPartialRDD.collect():
+        merge_partial_heatmap(session, heatmapPartial)
 
-def main(sc):
-    check_config()
+def merge_heatmap_partials(heatmapPartials):
+    heatmapPartials.foreachRDD(merge_partial_heatmap_rdd)
 
-    lines = sc.textFile(os.environ["LOCATIONS_ROOT"])
-    locationSlice = lines.flatMap(json_loader)
-    tileTimespanAggregates = locationSlice.flatMap(tile_id_timespans_mapper).reduceByKey(lambda total1,total2:total1+total2)
+MICROBATCH_FREQUENCY_SECONDS = 10
 
-    zoomLevelStats = tileTimespanAggregates.map(zoom_level_stats_mapper).reduceByKey(zoom_level_stats_reducer)
-    zoomLevelStats.foreachPartition(write_stats_into_blob_storage)
+def streamingMain(sc):
+    sc.stop()
+    conf = SparkConf()
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, MICROBATCH_FREQUENCY_SECONDS)
 
-    resultSets = tileTimespanAggregates.flatMap(key_by_result_set).groupByKey().mapValues(list)
-    resultSets.foreachPartition(write_result_set_into_blob_storage)
+    rows = KafkaUtils.createStream(ssc, os.environ["KAFKA_ENDPOINT"], 'heatmap-streaming', {"locations": 1})
+    locations = rows.flatMap(kafka_json_loader)
+    heatmap_partials = build_heatmaps(locations)
+    merge_heatmap_partials(heatmap_partials)
+
+    ssc.start()
+    ssc.awaitTermination()
+
+def batchMain(sc):
+    sc.stop()
+    conf = SparkConf(True).set("spark.cassandra.connection.host", CASSANDRA_ENDPOINT)
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+
+    rows = sqlContext.read.format("org.apache.spark.sql.cassandra")
+                          .options(table="locations", keyspace="fortis")
+                          .load()
+    
+    locations = rows.rdd.map(dataframe_loader)
+    heatmaps = build_heatmaps(locations)
+    heatmapsMaterialized = heatmaps.mapValues(heatmap_to_json)
+    heatmapDataFrames = sqlContext.createDataFrame(heatmapsMaterialized, ['id','heatmap'])
+    heatmapDataFrames.write.format("org.apache.spark.sql.cassandra")
+                           .mode('append')
+                           .options(table='heatmaps',keyspace='rhom')
+                           .save()
 
 if __name__ == "__main__":
     conf = SparkConf()
     sc = SparkContext(conf=conf)
-
-    main(sc)
+    if "USE_STREAMING" in os.environ and os.environ["USE_STREAMING"] == "true":
+        streamingMain(sc)
+    else:
+        batchMain(sc)
